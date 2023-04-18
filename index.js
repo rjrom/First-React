d6c2838231712c2bb893fdedafd320a256543ab8
rjrom <108962918+rjrom@users.noreply.github.com>
2023-04-18 10:44:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ function MainContent() {
       <div className="main-content">
           <h1>Reasons I'm excited to learn React</h1>
           <ol>
-              <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer if I know React</li>
+              <li>It's declarative</li>
+              <li>It's actively maintained by skilled people</li>
+              <li>It's a hireable skill</li>
           </ol>
       </div>
   )
